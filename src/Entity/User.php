f90959312076d5623fe8b28edb85aa7d0d9fc0d4
Thomas Dem <demagny.t@hotmail.fr>
2020-07-10 14:37:40 +0200

    update entity user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -18,126 +18,115 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=40)
+     * @ORM\Column(type="string", length=255)
      */
-    private $civility;
+    private $nickName;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $name;
+        private $passwd;
+
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $first_name;
+    private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $city;
+    private $lastName;
 
     /**
-     * @ORM\Column(type="string", length=20)
+     * @return int|null
      */
-    private $zipcode;
-
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $address;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $role;
-
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getCivility(): ?string
+
+
+    /**
+     * @return mixed
+     */
+    public function getNickName()
     {
-        return $this->civility;
+        return $this->nickName;
     }
 
-    public function setCivility(string $civility): self
+    /**
+     * @param mixed $nickName
+     */
+    public function setNickName($nickName): void
     {
-        $this->civility = $civility;
+        $this->nickName = $nickName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPasswd()
+    {
+        return $this->passwd;
+    }
+
+    /**
+     * @param mixed $passwd
+     */
+    public function setPasswd($passwd): void
+    {
+        $this->passwd = $passwd;
+    }
+
+    /**
+     * @param string $firstName
+     * @return $this
+     */
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
 
         return $this;
     }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
+    /**
+     * @return string|null
+     */
     public function getFirstName(): ?string
     {
-        return $this->first_name;
+        return $this->firstName;
     }
 
-    public function setFirstName(string $first_name): self
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName): void
     {
-        $this->first_name = $first_name;
-
-        return $this;
+        $this->lastName = $lastName;
     }
 
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
 
-    public function setCity(string $city): self
-    {
-        $this->city = $city;
 
-        return $this;
-    }
 
-    public function getZipcode(): ?string
-    {
-        return $this->zipcode;
-    }
 
-    public function setZipcode(string $zipcode): self
-    {
-        $this->zipcode = $zipcode;
 
-        return $this;
-    }
 
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
 
-    public function setAddress(string $address): self
-    {
-        $this->address = $address;
 
-        return $this;
-    }
 
-    public function getRole(): ?string
-    {
-        return $this->role;
-    }
 
-    public function setRole(string $role): self
-    {
-        $this->role = $role;
-
-        return $this;
-    }
 }
+
+
+
+
+
