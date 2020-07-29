678826dee5ad9e3465d9a1085d5e64c9478f403d
Gael <gael.gosse@yahoo.fr>
2020-07-29 16:43:47 +0200

    update day and night js/css

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -11,12 +11,11 @@ document.addEventListener('DOMContentLoaded', function(){
     var href = document.querySelectorAll('a');
     var input = document.querySelectorAll('input');
     var select = document.querySelectorAll('select');
+    var drop = document.getElementsByClassName('dropdown-menu');
 
-
-    nuit.addEventListener('click', function(){
+    function darkness(){
         body.classList.toggle('nuit');
-        nav.classList.toggle('bx-nuit')
-
+        nav.classList.toggle('bx-nuit');
         for(let i = 0; i < card.length; i++){
             card[i].classList.toggle('nuit-fonce');
         }
@@ -32,5 +31,33 @@ document.addEventListener('DOMContentLoaded', function(){
         for(let i = 0; i < select.length; i++){
             select[i].classList.toggle('nuit-fonce');
         }
+        for(let i = 0; i < drop.length; i++){
+            drop[i].classList.toggle('nuit-fonce')
+            drop[i].classList.toggle('bx-day')
+        }
+    }
+
+    if(localStorage['access'] == 'night'){
+        darkness();
+        nuit.checked = true
+        console.log('ca devrait marcher')
+    }
+
+    localStorage.setItem('access', 'day');
+    console.log(localStorage['access'])
+    
+    nuit.addEventListener('click', function(){
+        
+        if(localStorage['access'] == 'day'){
+            localStorage.removeItem('access', 'day');
+            localStorage.setItem('access', 'night');
+            darkness();
+        }
+        else{
+            localStorage.removeItem('access', 'night');
+            localStorage.setItem('access', 'day');
+        }
+        console.log(localStorage['access'])
+        
     })
 });
