22ea7769f65fc05473e956db42ec20d61b99ac12
Gael <gael.gosse@yahoo.fr>
2020-07-31 10:43:08 +0200

    modal connexion

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -59,7 +59,8 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     }
 
-    // CODE
+    /* *//* CODE *//* */
+
     localStorage.setItem('access', 'day');
     console.log('start page with: ' + localStorage['access'])
 
@@ -99,4 +100,27 @@ document.addEventListener('DOMContentLoaded', function(){
         console.log('end: ' + localStorage['access'])
         
     })
+
+    // MODAL
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    var connexion = document.getElementById('connexion');
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the link, open the modal 
+    connexion.addEventListener('click', function(){
+        modal.style.display = "block";
+    })
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
