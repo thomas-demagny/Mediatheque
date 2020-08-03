716fa044a8c397c8ddec1656f1fe3c135d1596f5
Gael <gael.gosse@yahoo.fr>
2020-08-03 15:26:38 +0200

    corrections

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,105 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', function(){
 
+// INIT VAR
+var nuit = document.getElementById('nuit');
+var body = document.querySelector('body');
+var nav = document.getElementById('mainNav');
+var card = document.getElementsByClassName('card-body');
+var tr = document.querySelectorAll('tr');
+var drop = document.getElementsByClassName('dropdown-menu')
+var href = document.querySelectorAll('a');
+var input = document.querySelectorAll('input');
+var select = document.querySelectorAll('select');
+
+// FUNCTIONS
+function darkness(){
+    body.classList.add('nuit');
+    nav.classList.add('bx-nuit');
+    for(let i = 0; i < card.length; i++){
+        card[i].classList.add('nuit-fonce');
+    }
+    for(let i = 0; i < tr.length; i++){
+        tr[i].classList.add('white');
+    }
+    for(let i = 0; i < href.length; i++){
+        href[i].classList.add('white');
+    }
+    for(let i = 0; i < input.length; i++){
+        input[i].classList.add('nuit-fonce');
+    }
+    for(let i = 0; i < select.length; i++){
+        select[i].classList.add('nuit-fonce');
+    }
+    for(let i = 0; i < drop.length; i++){
+        drop[i].classList.add('nuit-fonce')
+        drop[i].classList.add('bx-day')
+    }
+}
+function brightness(){
+    body.classList.remove('nuit');
+    nav.classList.remove('bx-nuit');
+    for(let i = 0; i < card.length; i++){
+        card[i].classList.remove('nuit-fonce');
+    }
+    for(let i = 0; i < tr.length; i++){
+        tr[i].classList.remove('white');
+    }
+    for(let i = 0; i < href.length; i++){
+        href[i].classList.remove('white');
+    }
+    for(let i = 0; i < input.length; i++){
+        input[i].classList.remove('nuit-fonce');
+    }
+    for(let i = 0; i < select.length; i++){
+        select[i].classList.remove('nuit-fonce');
+    }
+    for(let i = 0; i < drop.length; i++){
+        drop[i].classList.remove('nuit-fonce')
+        drop[i].classList.remove('bx-day')
+    }
+}
+
+/* *//* CODE *//* */
+
+localStorage.setItem('access', 'day');
+console.log('start page with: ' + localStorage['access'])
+
+// test after page loaded 'night' or 'day'
+if(localStorage['access'] == 'night'){
+    darkness();
+    nuit.checked = true
+    console.log(' ')
+    console.log('confirmation: ' + localStorage['access'])
+    console.log(' ')
+}
+else if(localStorage['access'] == 'day'){
+    brightness();
+    nuit.checked = false
+    console.log(' ')
+    console.log('confirmation: ' + localStorage['access'])
+    console.log(' ')
+}
+
+// listener on btn-toggle
+nuit.addEventListener('click', function(){
+    if(localStorage['access'] == 'day'){
+        console.log('ifDayBefore: ' + localStorage['access'])
+        localStorage.removeItem('access', 'day');
+        localStorage.setItem('access', 'night');
+        console.log('ifDayAfter: ' + localStorage['access'])
+        darkness();
+    }
+    else if(localStorage['access'] == 'night'){
+        console.log('night')
+        console.log('ifNigthBefore: ' + localStorage['access'])
+        localStorage.removeItem('access', 'night');
+        localStorage.setItem('access', 'day');
+        console.log('ifNightAfter: ' + localStorage['access'])
+        brightness();
+    }
+    console.log('end: ' + localStorage['access'])
     
+}) 
     
 });
