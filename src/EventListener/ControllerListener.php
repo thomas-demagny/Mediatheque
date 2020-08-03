5a90b6d066d35f0774efa877944076ad2ec18ec3
Gael <gael.gosse@yahoo.fr>
2020-08-03 14:45:08 +0200

    try to add event

--- src/EventListener/ControllerListener.php
+++ src/EventListener/ControllerListener.php
@@ -1,13 +1,32 @@
 <?php
 
-
 namespace App\EventListener;
 
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Psr\Log\LoggerInterface;
 
 class ControllerListener
 {
-    public function onKernelController($event){
+    public function __construct(LoggerInterface $logger){
+        
+        $this->logger = $logger;
 
-        //TODO mettre le corps de la function
+    }
+    
+    public function index(EventRepository $eventRepository): Response
+    {
+        return $this->render('event/index.html.twig', [
+            'event' => $eventRepository->findAll(),
+        ]);
+    }
+
+    public function onKernelController(ControllerEvent $event){
+
+        $content = $event->getController();
+
+        /*
+        var_dump($content);
+        $event->setArguments($newArguments);
+        */
     }
 }
